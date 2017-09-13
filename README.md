d9d2d0bd90d698ec5722c74bc95695c7c57f695d
Evan Liu <hmisty@gmail.com>
2017-09-13 12:08:54 +0800

    0.5.1 to publish

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bson-rpc-nodejs
 nodejs version of bson-rpc
+
+the python version is here: https://github.com/hmisty/bson-rpc
+
---
+++ examples/client.js
@@ -0,0 +1,15 @@
+var bson_rpc = require('../bson_rpc');
+
+var proxy = new bson_rpc('127.0.0.1', 8181);
+proxy.use_service(['add']);
+
+proxy.on_result((err, result) => {
+	if (err) throw err;
+	console.log('result is ' + result);
+	proxy.disconnect();
+});
+	
+proxy.connect(() => {
+	console.log('connected');
+	proxy.add(1, 2);
+});
