4ca540e5934252f42657c98416acb4268f67ef3a
Evan Liu <hmisty@gmail.com>
2017-09-27 22:12:43 +0800

    add func loop

--- examples/client.js
+++ examples/client.js
@@ -3,10 +3,11 @@ var bson_rpc = require('..');
 var proxy = new bson_rpc.client('127.0.0.1', 8181);
 proxy.use_service(['add']);
 
+var count = 0;
 proxy.on_result((err, result) => {
 	if (err) throw err;
 	console.log('result is ' + result);
-	proxy.disconnect();
+	count++;
 });
 	
 proxy.connect(() => {
@@ -16,3 +17,14 @@ proxy.connect(() => {
 	proxy.add(1, 2);
 	proxy.add(1, 2);
 });
+
+function loop() {
+	if (count >= 4) {
+		proxy.disconnect();
+	} else {
+		setTimeout(loop, 100);
+	}
+};
+
+loop();
+//proxy.disconnect();
