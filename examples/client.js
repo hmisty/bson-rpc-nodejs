84f422fc492b44562b0dab27c195074aa90e4ee2
Evan Liu <hmisty@gmail.com>
2017-09-27 23:14:05 +0800

    compose chunks and decode bson obj

--- examples/client.js
+++ examples/client.js
@@ -1,30 +1,24 @@
+var util = require('util');
 var bson_rpc = require('..');
 
 var proxy = new bson_rpc.client('127.0.0.1', 8181);
 proxy.use_service(['add']);
 
-var count = 0;
-proxy.on_result((err, result) => {
-	if (err) throw err;
-	console.log('result is ' + result);
-	count++;
+proxy.connect();
+
+proxy.on_result(proxy.add(1, 2), function (err, doc) {
+	console.log(doc);
 });
-	
-proxy.connect(() => {
-	console.log('connected');
-	proxy.add(1, 2);
-	proxy.add(1, 2);
-	proxy.add(1, 2);
-	proxy.add(1, 2);
+proxy.on_result(proxy.add(3, 4), function (err, doc) {
+	console.log(doc);
+});
+proxy.on_result(proxy.add(5, 6), function (err, doc) {
+	console.log(doc);
+});
+proxy.on_result(proxy.add(7, 8), function (err, doc) {
+	console.log(doc);
+	proxy.disconnect();
 });
 
-function loop() {
-	if (count >= 4) {
-		proxy.disconnect();
-	} else {
-		setTimeout(loop, 100);
-	}
-};
-
-loop();
-//proxy.disconnect();
+//console.log(util.inspect(proxy.obj_received));
+//console.log(util.inspect(proxy.callbacks));
