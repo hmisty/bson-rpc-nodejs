797036098c02ba95c9c1b553ff5eb4bea2fef17f
Evan Liu <hmisty@gmail.com>
2017-09-27 23:49:19 +0800

    add callback for connect

--- examples/client.js
+++ examples/client.js
@@ -4,20 +4,24 @@ var bson_rpc = require('..');
 var proxy = new bson_rpc.client('127.0.0.1', 8181);
 proxy.use_service(['add']);
 
-proxy.connect();
+function start() {
+	proxy.on_result(proxy.add(1, 2), function (err, doc) {
+		console.log(doc);
+	});
+	proxy.on_result(proxy.add(3, 4), function (err, doc) {
+		console.log(doc);
+	});
+	proxy.on_result(proxy.add(5, 6), function (err, doc) {
+		console.log(doc);
+	});
+	proxy.on_result(proxy.add(7, 8), function (err, doc) {
+		console.log(doc);
+		proxy.disconnect();
+	});
+}
 
-proxy.on_result(proxy.add(1, 2), function (err, doc) {
-	console.log(doc);
-});
-proxy.on_result(proxy.add(3, 4), function (err, doc) {
-	console.log(doc);
-});
-proxy.on_result(proxy.add(5, 6), function (err, doc) {
-	console.log(doc);
-});
-proxy.on_result(proxy.add(7, 8), function (err, doc) {
-	console.log(doc);
-	proxy.disconnect();
+proxy.connect(() => {
+						 start();
 });
 
 //console.log(util.inspect(proxy.obj_received));
