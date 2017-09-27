bfaba6a4e4fb3463df7a17ad4c135f5ef6ecc1b4
Evan Liu <hmisty@gmail.com>
2017-09-27 23:31:49 +0800

    refined client2 example

--- examples/client2.js
+++ examples/client2.js
@@ -8,7 +8,7 @@ var failure = 0;
 var proxy = new bson_rpc.client('127.0.0.1', 8181);
 proxy.use_service(['hi', 'echo', 'add']);
 
-proxy.on_result((err, result) => {
+on_result_func = (err, result) => {
 	if (err) throw err;
 	
 	if (result == 3) { //1+2=3
@@ -19,30 +19,29 @@ proxy.on_result((err, result) => {
 	}
 
 	if (keep_running) {
-		proxy.add(1, 2);
+		proxy.on_result(proxy.add(1, 2), on_result_func);
 	} else {
 		proxy.disconnect();
 	}
-});
 
-proxy.connect(() => {
-	console.log('connected');
+};
 
-	var time_to_run = 5 * 1000; // 5 seconds
-	var start = Date.now();
-	setTimeout(() => {
-		keep_running = false;
+proxy.connect();
 
-		var elapsed = Date.now() - start;
-		console.log('Time elapsed: ' + elapsed + ' ms');
-		console.log('Successful requests: ' + success);
-		console.log('Failed requests: ' + failure);
-		console.log('Request per second: ' + Math.floor(success / elapsed * 1000));
-	}, time_to_run); //stop after predefined seconds
+var time_to_run = 5 * 1000; // 5 seconds
+var start = Date.now();
+setTimeout(() => {
+	keep_running = false;
 
-	var workers = 1; //20;
-	for (var i = 0; i < workers; i++) {
-		proxy.add(1, 2);
-	}
+	var elapsed = Date.now() - start;
+	console.log('Time elapsed: ' + elapsed + ' ms');
+	console.log('Successful requests: ' + success);
+	console.log('Failed requests: ' + failure);
+	console.log('Request per second: ' + Math.floor(success / elapsed * 1000));
+}, time_to_run); //stop after predefined seconds
 
-});
+var workers = 1; //20;
+for (var i = 0; i < workers; i++) {
+	proxy.on_result(proxy.add(1, 2), on_result_func);
+}
+
